ab93f540ed2ca2041828f8d9ef0eaa9cb853bd5c
Gustavo Pereira <gustavomiranda04dg@gmail.com>
2023-10-03 16:01:30 -0300

    concluí o primeiro desafio

--- desafio/script.js
+++ desafio/script.js
@@ -1,20 +1,21 @@
-function hora(){
+function carregar() {
+    var msg = window.document.getElementById('hrdia')
+    var img = window.document.getElementById('ft')
     var agora = new Date()
-    var hora = agora.getHours
-    var hrtxt = document.querySelector('div#hrdia') 
-    var imag = document.querySelector('img#ft')
-
-    if (hora <= 4 && hora < 12){
-        hrtxt.innerTxt = `Agora são ${hora} horas da manhã`
-        imag.style.backgroun= 'url(../imagens/manha.jpg)'
-    } else if (hora < 18){
-        hrtxt.innerTxt = `Agora são ${hora} horas da tarde`
-        imag.style.backgroun= 'url(../imagens/tarde.jpg)'
-    } else if (hora <= 24) {
-        hrtxt.innerTxt = `Agora são ${hora} horas da noite`
-        imag.style.backgroun= 'url(../imagens/noite.jpg)'
+    var hora = agora.getHours()
+    msg.innerHTML = `Agora são ${hora}`
+    
+    if (hora >= 4 && hora < 12){
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#78bce9'
+    } else if (hora >= 12 && hora <18){
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#e9b878'
+    } else if (hora >= 18 && hora <= 24){
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#101638'
     } else {
-        hrtxt.innerTxt = `Agora são ${hora} horas da madrugada`
-        imag.style.backgroun= 'url(../imagens/madrugada.jpg)'
+        img.src = 'imagens/madrugada.jpg'
+        document.body.style.background = '#030511'
     }
 }
