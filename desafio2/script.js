51efab54032a271dca544cc40d55d80f4763c28a
Gustavo Pereira <gustavomiranda04dg@gmail.com>
2023-10-09 17:39:16 -0300

    conclui o segundo desafio

--- desafio2/script.js
+++ desafio2/script.js
@@ -3,15 +3,47 @@ function verificar(){
     var anoAtual = agora.getFullYear()  
     var ano = document.getElementById('iano')
     var res = document.querySelector('div#res')
-    var idade = anoAtual - Number(ano.value)
-    var fsex = document.getElementsByName('resex')
     
     if (ano.value.length == 0 || Number(ano.value) > anoAtual){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } 
+    } else {
+        var fsex = document.getElementsByName('resex')
+        var idade = anoAtual - Number(ano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
 
-    switch escrita(){
-        
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >=0 && idade < 10){
+                //garoto
+                img.setAttribute('src', 'imagens/garoto.png')
+            } else if (idade < 50){
+                //homem
+                img.setAttribute('src', 'imagens/homem.png')
+            } else{
+                //idoso
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
+
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >=0 && idade < 10){
+                //garota
+                img.setAttribute('src', 'imagens/garota.png')
+            } else if (idade < 50){
+                //mulher
+                img.setAttribute('src', 'imagens/mulher.png')
+            } else{
+                //idosa
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos que você é ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
+
+    
 }
 
